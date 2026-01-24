28ec0a875cad7c35f1bd922285194f283ac137ed
Ranjan Kumar Singh <137ranjan@gmail.com>
2026-01-24 16:01:27 +0530

    Create minimal BST

--- src/main/java/com/ranjan/ctci/Main.java
+++ src/main/java/com/ranjan/ctci/Main.java
@@ -1,34 +1,18 @@
 package com.ranjan.ctci;
 
-import com.ranjan.ctci.treesandgraphs.GraphNode;
-import com.ranjan.ctci.treesandgraphs.RouteBetweenNodes;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.ranjan.ctci.treesandgraphs.MinimalTree;
+import com.ranjan.ctci.treesandgraphs.TreeNode;
+import com.ranjan.ctci.treesandgraphs.TreeTraversals;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<GraphNode> graph = new ArrayList<>();
-        GraphNode firstNode = new GraphNode();
-        GraphNode secondNode = new GraphNode();
-        GraphNode thirdNode = new GraphNode();
-        GraphNode fourthNode = new GraphNode();
-        GraphNode fifthNode = new GraphNode();
-        GraphNode sixthNode = new GraphNode();
-
-        fifthNode.adjacent.add(sixthNode);
-        thirdNode.adjacent.add(fourthNode);
-        secondNode.adjacent.add(thirdNode);
-        secondNode.adjacent.add(fourthNode);
-        firstNode.adjacent.add(secondNode);
-
-        graph = Arrays.asList(firstNode, secondNode, thirdNode, fourthNode, fifthNode, sixthNode);
-
-        RouteBetweenNodes routeBetweenNodes = new RouteBetweenNodes();
-        System.out.println(routeBetweenNodes.search(graph, firstNode, fifthNode));
-
-
+        int[] sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20};
+        MinimalTree minimalTree = new MinimalTree();
+        TreeNode root = minimalTree.createMinimalBST(sortedArray);
+        TreeTraversals treeTraversals = new TreeTraversals();
+        treeTraversals.inOrderTraversal(root);
+        System.out.println();
+        treeTraversals.postOrderTraversal(root);
     }
 }
---
+++ src/main/java/com/ranjan/ctci/treesandgraphs/MinimalTree.java
@@ -0,0 +1,19 @@
+package com.ranjan.ctci.treesandgraphs;
+
+public class MinimalTree {
+
+    public TreeNode createMinimalBST(int[] array) {
+        return createMinimalBST(array, 0, array.length - 1);
+    }
+
+    private TreeNode createMinimalBST(int[] arr, int start, int end) {
+        if (end < start) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+        TreeNode node = new TreeNode(arr[mid]);
+        node.left = createMinimalBST(arr, start, mid - 1);
+        node.right = createMinimalBST(arr, mid + 1, end);
+        return node;
+    }
+}
